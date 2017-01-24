d508d1a524162b4146466519529cb654e0d7e9f0
BRUNO-DELL14R\Bruno <bruno.souza.siueg@gmail>
2017-01-24 18:00:38 -0200

    Embedded

---
+++ src/main/java/com/hunters/EmbeddedWebServer.java
@@ -0,0 +1,32 @@
+package com.copperykeenclaws;
+
+import java.net.URL;
+import java.security.ProtectionDomain;
+
+import org.apache.catalina.startup.Tomcat;
+
+public class EmbeddedWebServer {
+
+	private static final long serialVersionUID = -6777759990550225380L;
+
+	public static void main(String[] args) throws Exception {
+
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
+
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(Integer.parseInt(webPort));
+
+		// Load the war (assumes this class in in root of war file)
+		final ProtectionDomain domain = EmbeddedWebServer.class
+				.getProtectionDomain();
+		final URL location = domain.getCodeSource().getLocation();
+		tomcat.addWebapp("/", location.toURI().getPath());
+
+		// Start server
+		tomcat.start();
+		tomcat.getServer().await();
+	}
+}
